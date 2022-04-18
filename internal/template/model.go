f468cfac8d0452f5fe75d5d8c590034f37167d37
mlazebny <miskadl09@gmail.com>
2022-04-18 22:19:41 +0300

    rm useless models

--- internal/template/model.go
+++ internal/template/model.go
@@ -1,7 +1,5 @@
 package template
 
-import "golang.org/x/crypto/bcrypt"
-
 type User struct {
 	ID       string `json:"id"`
 	Username string `json:"username"`
@@ -15,29 +13,6 @@ func (u *CreateUserHashedDTO) NewUser() *User {
 		Email:    u.Email,
 		Password: string(u.Password),
 	}
-}
-
-func (u *UpdateUserDTO) NewUser(oldUser *User) (*User, error) {
-	ep, err := bcrypt.GenerateFromPassword([]byte(u.NewPassword), bcrypt.DefaultCost)
-	if err != nil {
-		return &User{}, nil
-	}
-	user := &User{
-		Username: u.Username,
-		Email:    u.Email,
-		Password: string(ep),
-		ID:       u.ID,
-	}
-	if user.Username == "" {
-		user.Username = oldUser.Username
-	}
-	if user.Email == "" {
-		user.Email = oldUser.Email
-	}
-	if user.Password == "" {
-		user.Password = oldUser.Password
-	}
-	return user, nil
 }
 
 func (u *CreateUserDTO) Hashed(hashedPassword []byte) *CreateUserHashedDTO {
@@ -58,18 +33,4 @@ type CreateUserHashedDTO struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password []byte `json:"password"`
-}
-
-type GetUserByEmailAndPasswordDTO struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type UpdateUserDTO struct {
-	ID          string `json:"id" validate:"required"`
-	Username    string `json:"username,omitempty" `
-	Email       string `json:"email,omitempty"`
-	Password    string `json:"password,omitempty"`
-	OldPassword string `json:"old_password,omitempty"`
-	NewPassword string `json:"new_password,omitempty"`
 }
